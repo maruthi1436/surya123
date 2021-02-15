2950e19f2bc0d81bfbdfb31055e5bdbecbbede2b
maruthi1436 <61113894+maruthi1436@users.noreply.github.com>
2021-02-15 13:36:13 +0530

    update sisi.sh

--- sisi.sh
+++ sisi.sh
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-my name is sisindri
-i am stuying i hyderbad 
-am love with you 
-=======
- i am studying in mca in hyderabad
 
->>>>>>> sisi
-=======
-my name is sisindri
->>>>>>> maruthi
+ i am studying in mca in hyderabad
+ sisi
+my name is sisind
+maruthi
